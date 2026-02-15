73219996b66b001f028b6ffbff76258bb268942d
Danila <braun.kc@yandex.ru>
2026-02-15 12:46:18 +0300

    feat(auth): added limits for db conns

--- auth-service/internal/infra/database/db.go
+++ auth-service/internal/infra/database/db.go
@@ -2,6 +2,7 @@ package database
 
 import (
 	"fmt"
+	"time"
 
 	"github.com/braunkc/ai-bot-constructor/auth-service/config"
 	"gorm.io/driver/postgres"
@@ -15,6 +16,14 @@ func New(cfg *config.DBConfig) (*gorm.DB, error) {
 	if err != nil {
 		return nil, fmt.Errorf("failed to connect to db: %w", err)
 	}
+
+	sqlDB, err := db.DB()
+	if err != nil {
+		return nil, err
+	}
+	sqlDB.SetMaxOpenConns(50)
+	sqlDB.SetMaxIdleConns(5)
+	sqlDB.SetConnMaxLifetime(time.Hour)
 
 	return db, nil
 }
